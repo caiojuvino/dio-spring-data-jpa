8e8bffc70dd562128f9934ecee5fd029543511a1
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-04-06 11:30:47 -0300

    Finalizando

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/service/IMatriculaService.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/service/IMatriculaService.java
@@ -24,7 +24,7 @@ public interface IMatriculaService {
    * Retorna todas as Matrículas que estão no banco de dados.
    * @return - uma lista com todas as Matrículas que estão salvas no DB.
    */
-  List<Matricula> getAll();
+  List<Matricula> getAll(String Bairro);
 
   /**
    * Deleta uma Matrícula específica.
